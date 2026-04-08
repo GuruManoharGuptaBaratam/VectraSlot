1be8bb002f1045ec825224c0cbb91574a609f527
Harsith Priyan Sivanandham <harsithpriyan@gmail.com>
2026-04-08 14:05:02 +0530

    Added Middlewares - S. Harsith Priyan

---
+++ backend/src/middlewares/admin.middleware.ts
@@ -0,0 +1,14 @@
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "./auth.middleware";
+
+export const adminMiddleware = (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction,
+) => {
+    if (!req.user || req.user.role !== "ADMIN") {
+        return res.status(403).json({ message: "Forbidden: Admin only" });
+    }
+
+    next();
+};
