6351c809b22e6ba475ec0bc3479b2089651b2926
GuruManoharGuptaBaratam <manoharguptabaratam@gmail.com>
2026-04-29 11:41:31 +0530

    cros erros fixing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -86,7 +86,6 @@ class App {
         };
 
         this.app.use(cors(corsOptions));
-        this.app.options("*", cors(corsOptions));
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
     }
